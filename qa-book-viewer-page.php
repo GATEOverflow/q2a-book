84c777d87c436e681497b3ea50c276104b328b72
Arjun <arjunsuresh1987@gmail.com>
2026-06-08 13:49:58 +0530

    Fix for code layout in bookviewer

--- qa-book-viewer-page.php
+++ qa-book-viewer-page.php
@@ -87,7 +87,7 @@ class qa_book_page
 			$sel = ($slug === $selectedBook) ? ' selected' : '';
 			$bookOptions .= '<option value="' . qa_html($slug) . '"' . $sel . '>' . qa_html($title) . '</option>';
 		}
-		$version = 12;
+		$version = 13;
 
 		$rootUrl = qa_path_html('book');
 		$ajaxUrl = qa_path_html('book-ajax');
